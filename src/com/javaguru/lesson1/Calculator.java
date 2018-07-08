725bc61471292168962c0ebbd06eeb0b3bdb78ba
Ruslans Tufetulovs <ruslans.tufetulovs@gmail.com>
2018-07-08 10:49:27 +0300

    Calculator demo

---
+++ src/com/javaguru/lesson1/Calculator.java
@@ -0,0 +1,9 @@
+package com.javaguru.lesson1;
+
+class Calculator {
+
+    public int sum(int firstNumber, int secondNumber) {
+        int sumResult = firstNumber + secondNumber;
+        return sumResult;
+    }
+}
---
+++ src/com/javaguru/lesson1/CalculatorDemo.java
@@ -0,0 +1,10 @@
+package com.javaguru.lesson1;
+
+class CalculatorDemo {
+
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        int sumResult = calc.sum(10,20);
+        System.out.println(sumResult);
+    }
+}
