c09693069146574675fb18fd581e63361cf3768e
Ruslans Tufetulovs <ruslans.tufetulovs@gmail.com>
2018-07-08 10:08:34 +0300

    Variables part2

--- src/com/javaguru/lesson1/Variables.java
+++ src/com/javaguru/lesson1/Variables.java
@@ -3,7 +3,32 @@ package com.javaguru.lesson1;
 class Variables {
 
     public static void main(String[] args) {
-        int integerNumber = 20;
+        int integerNumber = 20213;
         System.out.println(integerNumber);
+
+        byte byteNumber = 13;
+        System.out.println(byteNumber);
+
+        short shortNumber = 145;
+        System.out.println(shortNumber);
+
+        long longNumber = 2134423;
+        System.out.println(longNumber);
+
+        float floatNumber = 10.5f;
+        System.out.println(floatNumber);
+
+        double doubleNumber = 12.12345678901234;
+        System.out.println(doubleNumber);
+
+        boolean bool = false;
+        System.out.println(bool);
+
+        char charVariable = 'a';
+        System.out.println(charVariable);
+
+        char charNumber = 83;
+        System.out.println(charNumber);
+
     }
 }
