fb281a20121a4bc4978b9bfb8379c43aa207f7cd
Ruslans Tufetulovs <ruslans.tufetulovs@gmail.com>
2018-08-19 10:24:41 +0300

    Example do while

---
+++ src/com/javaguru/lesson6/ExampleDoWhile.java
@@ -0,0 +1,12 @@
+package com.javaguru.lesson6;
+
+class ExampleDoWhile {
+
+    public static void main(String[] args) {
+        int counter = 0;
+        do {
+            System.out.println(counter);
+            counter++;
+        } while (counter <= 100);
+    }
+}
