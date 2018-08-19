1ebb6944e8279042c53e71b19e537d0280f73efd
Ruslans Tufetulovs <ruslans.tufetulovs@gmail.com>
2018-08-19 10:22:52 +0300

    Example for loop

---
+++ src/com/javaguru/lesson6/ExampleForLoop.java
@@ -0,0 +1,10 @@
+package com.javaguru.lesson6;
+
+class ExampleForLoop {
+
+    public static void main(String[] args) {
+        for (int i = 0; i <= 100; i++) {
+            System.out.print(i + " ");
+        }
+    }
+}
