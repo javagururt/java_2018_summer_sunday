1fcf9b1e3a72621010e14368df6af2541b9b9368
Ruslans Tufetulovs <ruslans.tufetulovs@gmail.com>
2018-09-23 10:12:07 +0300

    Exception example

---
+++ src/com/javaguru/lesson11/ExceptionExample.java
@@ -0,0 +1,29 @@
+package com.javaguru.lesson11;
+
+import java.util.Random;
+
+class ExceptionExample {
+
+    public static void main(String[] args) {
+        try {
+            throwRuntimeException();
+            throwException();
+        } catch (RuntimeException e) {
+            System.out.println(e.getMessage());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void throwException() throws Exception {
+        throw new Exception("Checked exception");
+    }
+
+    private static void throwRuntimeException() {
+        Random random = new Random();
+        int number = random.nextInt(2);
+        if (number == 0) {
+            throw new RuntimeException("Unchecked exception");
+        }
+    }
+}
